f34c5493fe3b16cc2cfdffe08566c7ec26a49961
Danilo Juvinao <proyectos@ibcaribe.com>
2021-10-28 19:24:33 -0500

    nueva clase y prueba

---
+++ src/main/java/com/mycompany/gha/g26/Clase2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gha.g26;
+
+/**
+ *
+ * @author juvinao
+ */
+public class Clase2 {
+
+    public double valorIncrementado(double base, double porc) {
+        return base + base * porc / 100;
+    }
+}
---
+++ src/test/java/com/mycompany/gha/g26/Clase2Test.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gha.g26;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author juvinao
+ */
+public class Clase2Test {
+
+    public Clase2Test() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of valorIncrementado method, of class Clase2.
+     */
+    @Test
+    public void testValorIncrementado() {
+        Clase2 instance = new Clase2();
+        double result = instance.valorIncrementado(100, 30);
+        assertEquals(130, result);
+    }
+
+}
